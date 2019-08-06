589e6eac41c15392fac392a4129cc9775bd28fe7
FrolovOlegVladimirovich <frolovolegvladimirovich@gmail.com>
2019-08-06 16:21:39 +0300

    2. Сортировка User с использованием Comparator[#139260]

--- chapter_003/src/main/java/ru/job4j/sorting/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/sorting/SortUser.java
@@ -1,12 +1,14 @@
 package ru.job4j.sorting;
 
 import ru.job4j.search.Person;
+
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
 /**
- * Сортировка Person по возрасту.
+ * Сортировки Person.
  *
  * @author Oleg Frolov (frolovolegvladimirovich@gmail.com)
  * @since 06.08.2019
@@ -21,7 +23,41 @@ public class SortUser {
      * @param users List пользователей Person.
      * @return Отсортированный по возрасту TreeSet пользователей Person.
      */
-    public Set<Person> sort(List<Person> users) {
+    public Set<Person> sortAge(List<Person> users) {
         return new TreeSet<>(users);
+    }
+
+    /**
+     * Сортировка пользователей Person по длине имени.
+     *
+     * @param users List пользователей Person.
+     * @return Отсортированный по длине имени List пользователей Person.
+     */
+    public List<Person> sortNameLength(List<Person> users) {
+        Comparator<Person> compareNameLength = new Comparator<>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                return Integer.compare(o1.getName().length(), o2.getName().length());
+            }
+        };
+        users.sort(compareNameLength);
+        return users;
+    }
+
+    /**
+     * Сортировка по имени, потом по возрасту.
+     * @param users List пользователей Person.
+     * @return Отсортированный по имени, потом по возрасту, List пользователей Person.
+     */
+    public List<Person> sortAllFields(List<Person> users) {
+        Comparator<Person> compareNameAndAge = new Comparator<>() {
+            @Override
+            public int compare(Person o1, Person o2) {
+                int result = o1.getName().compareTo(o2.getName());
+                return result == 0 ? o1.getAge().compareTo(o2.getAge()) : result;
+            }
+        };
+        users.sort(compareNameAndAge);
+        return users;
     }
 }
--- chapter_003/src/test/java/ru/job4j/sorting/SortUserTest.java
+++ chapter_003/src/test/java/ru/job4j/sorting/SortUserTest.java
@@ -15,8 +15,11 @@ import static org.junit.Assert.assertThat;
  */
 public class SortUserTest {
 
+    /**
+     * Тест сортировки пользователей по возрасту.
+     */
     @Test
-    public void testSortingByAgeThenSortingByName() {
+    public void testSortingByAgeAfterSortingByName() {
         SortUser sortedUsers = new SortUser();
         List<Person> persons = new ArrayList<>(Arrays.asList(
                 new Person("Petr", "35"),
@@ -29,7 +32,7 @@ public class SortUserTest {
                 new Person("Polina", "7"),
                 new Person("Alena", "31")
         ));
-        Set<Person> result = sortedUsers.sort(persons);
+        Set<Person> result = sortedUsers.sortAge(persons);
         Set<Person> expected = new TreeSet<>(Arrays.asList(
                 new Person("Polina", "7"),
                 new Person("Vasily", "25"),
@@ -40,6 +43,70 @@ public class SortUserTest {
                 new Person("Petr", "35"),
                 new Person("Vitaly", "38"),
                 new Person("Nikolay", "44")
+        ));
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Тест сортировки пользователей по длине имени.
+     */
+    @Test
+    public void testSortingByNameLength() {
+        SortUser users = new SortUser();
+        List<Person> persons = new ArrayList<>(Arrays.asList(
+                new Person("Oleg"),
+                new Person("Vsevolod"),
+                new Person("Eva"),
+                new Person("Alena"),
+                new Person("Nikolay"),
+                new Person("Viktoriya"),
+                new Person("Polina")
+
+        ));
+        List<Person> result = users.sortNameLength(persons);
+        List<Person> expected = new ArrayList<>(Arrays.asList(
+                new Person("Eva"),
+                new Person("Oleg"),
+                new Person("Alena"),
+                new Person("Polina"),
+                new Person("Nikolay"),
+                new Person("Vsevolod"),
+                new Person("Viktoriya")
+        ));
+        assertThat(result, is(expected));
+    }
+
+    /**
+     * Тест сортировки по имени, потом по возрасту.
+     */
+    @Test
+    public void testSortingByNameAfterSortingByAge() {
+        SortUser users = new SortUser();
+        List<Person> persons = new ArrayList<>(Arrays.asList(
+                new Person("Oleg", "31"),
+                new Person("Vsevolod", "38"),
+                new Person("Viktoriya", "45"),
+                new Person("Eva", "31"),
+                new Person("Viktoriya", "15"),
+                new Person("Alena", "25"),
+                new Person("Oleg", "20"),
+                new Person("Nikolay", "44"),
+                new Person("Viktoriya", "31"),
+                new Person("Polina", "7")
+
+        ));
+        List<Person> result = users.sortAllFields(persons);
+        List<Person> expected = new ArrayList<>(Arrays.asList(
+                new Person("Alena", "25"),
+                new Person("Eva", "31"),
+                new Person("Nikolay", "44"),
+                new Person("Oleg", "20"),
+                new Person("Oleg", "31"),
+                new Person("Polina", "7"),
+                new Person("Viktoriya", "15"),
+                new Person("Viktoriya", "31"),
+                new Person("Viktoriya", "45"),
+                new Person("Vsevolod", "38")
         ));
         assertThat(result, is(expected));
     }
