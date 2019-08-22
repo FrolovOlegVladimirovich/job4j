636b28842f5d82513c061833e08e64e1940e7287
FrolovOlegVladimirovich <frolovolegvladimirovich@gmail.com>
2019-08-22 22:45:14 +0300

    Крестики-нолики на JavaFX[#139172]

---
+++ tictactoe/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -0,0 +1,55 @@
+package ru.job4j.tictactoe;
+
+import java.util.Arrays;
+import java.util.function.Predicate;
+
+/**
+ * Класс отвечает за проверку логики.
+ */
+public class Logic3T {
+    private final Figure3T[][] table;
+
+    public Logic3T(Figure3T[][] table) {
+        this.table = table;
+    }
+
+    public boolean fillBy(Predicate<Figure3T> predicate, int startX, int startY, int deltaX, int deltaY) {
+        boolean result = true;
+        for (int index = 0; index != this.table.length; index++) {
+            Figure3T cell = this.table[startX][startY];
+            startX += deltaX;
+            startY += deltaY;
+            if (!predicate.test(cell)) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * проверяет есть ли в поле выигрышные комбинации.
+     * @param predicate проверяет наличие фигуры в клетке.
+     * @return true, если есть выигрышная комбинация для Нолика или Крестика (в зависимости от параметра).
+     */
+    public boolean isWinner(Predicate<Figure3T> predicate) {
+        return this.fillBy(predicate, 0, 0, 1, 0)
+                || this.fillBy(predicate, 0, 0, 0, 1)
+                || this.fillBy(predicate, 0, 0, 1, 1)
+                || this.fillBy(predicate, this.table.length - 1, 0, -1, 1)
+                || this.fillBy(predicate, 0, 1, 1, 0)
+                || this.fillBy(predicate, 0, 2, 1, 0)
+                || this.fillBy(predicate, 1, 0, 0, 1)
+                || this.fillBy(predicate, 2, 0, 0, 1);
+    }
+
+    /**
+     * проверяет, если ли пустые клетки для новых ходов.
+     * @return false, если все поля заполнены.
+     */
+    public boolean hasGap() {
+        return Arrays.stream(this.table)
+                .flatMap(Arrays::stream)
+                .anyMatch(cell -> !(cell.hasMarkX() || cell.hasMarkO()));
+    }
+}
