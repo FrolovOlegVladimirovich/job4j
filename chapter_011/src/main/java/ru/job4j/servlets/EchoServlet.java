031de290fc67ec9dd9af84f522f1177e6726e03d
FrolovOlegVladimirovich <frolovolegvladimirovich@gmail.com>
2020-02-03 19:26:49 +0300

    HTTP protocol: doGet, doPost

--- chapter_011/src/main/java/ru/job4j/servlets/EchoServlet.java
+++ chapter_011/src/main/java/ru/job4j/servlets/EchoServlet.java
@@ -1,17 +1,29 @@
 package ru.job4j.servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class EchoServlet extends HttpServlet {
+    private List<String> users = new CopyOnWriteArrayList<>();
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
         PrintWriter writer = new PrintWriter(resp.getOutputStream());
-        writer.append("Hello world!");
+        writer.append(String.format("Hello world, %s!", users));
         writer.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        users.add(req.getParameter("login"));
+        doGet(req, resp);
     }
 }
